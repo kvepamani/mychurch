7b8487154886d051b4d08a03dcd4724e5aeb1d96
kvepamani <kvepamani@gmail.com>
2024-04-02 14:41:13 +0100

    dellete in PR

--- src/main/java/com/myc/MyChurchApplication.java
+++ src/main/java/com/myc/MyChurchApplication.java
@@ -4,6 +4,10 @@ public class MyChurchApplication {
     private void hello(String s) {
         System.out.println("I am in new file but dont want to delete this");
     }
+
+    private String myVillage(String name){
+        return name;
+    }
     public static void main(String[] args) {
         System.out.println("Welcome to mchurch application");
     }
