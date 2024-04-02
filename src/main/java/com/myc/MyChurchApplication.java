f39e733e5bb34e620b28ed243ab3e66ebafcfbc1
kvepamani <kvepamani@gmail.com>
2024-04-02 14:51:50 +0100

    Removed a modified file from pull request

--- src/main/java/com/myc/MyChurchApplication.java
+++ src/main/java/com/myc/MyChurchApplication.java
@@ -4,10 +4,6 @@ public class MyChurchApplication {
     private void hello(String s) {
         System.out.println("I am in new file but dont want to delete this");
     }
-
-    private String myVillage(String name){
-        return name;
-    }
     public static void main(String[] args) {
         System.out.println("Welcome to mchurch application");
     }
