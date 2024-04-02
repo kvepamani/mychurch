3aa4e2b0533e875676538b276a2803a261089a5e
kvepamani <kvepamani@gmail.com>
2024-04-02 14:31:05 +0100

    Test

--- src/main/java/com/myc/MyChurchApplication.java
+++ src/main/java/com/myc/MyChurchApplication.java
@@ -1,6 +1,9 @@
 package com.myc;
 
 public class MyChurchApplication {
+    private void hello(String s) {
+        System.out.println("I am in new file but dont want to delete this");
+    }
     public static void main(String[] args) {
         System.out.println("Welcome to mchurch application");
     }
